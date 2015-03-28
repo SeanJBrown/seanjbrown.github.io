eba85539c01873a00ab60e29b9dc3956d7da2476
SeanJBrown <seanbrowns760@gmail.com>
2015-03-28 14:05:12 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html> 
+<head> </head>
+	<body>
+		<script>
+			document.write("Hello World");
+		</script>\
+function add_one(input){
+	return input + 1
+}
+//this function counts to three
+functioncountTothree(upperbound) {
+	var i;
+
+	for(i = 0; i<3++) {
+			console.log(i);
+	}
+}
+	 </body>
